c9ca9836b5deae873794eb3ee5c38e07b6369f11
alextjj <254334386@qq.com>
2022-08-29 17:51:17 +0800

    proto build to .net

--- scripts/build-proto.sh
+++ scripts/build-proto.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 set -e
 
-protoc --go_out=. ./proto/*.proto
+readonly PROTO_ROOT="./proto" 
+protoc --go_out=. ${PROTO_ROOT}/*.proto
 
+
+# ------------- create dotnet use proto file ----------------
+readonly PROTO_NET_OUT="./messageDotNet/proto"
+readonly PROTO_BUILD_NET="./messageDotNet/protoNet"
+rm -rf ${PROTO_BUILD_NET}
+cp -r ${PROTO_ROOT} ${PROTO_BUILD_NET}
+sed -i -e '/go_package/d' ${PROTO_BUILD_NET}/*.proto 
+# sed -i -e 's/package .*/package MelandGame3;/g' ${PROTO_BUILD_NET}/*.proto
+protoc --proto_path=${PROTO_BUILD_NET} --csharp_out=${PROTO_NET_OUT}  ${PROTO_BUILD_NET}/*.proto
+rm -rf ${PROTO_BUILD_NET}
+
+ 

a9ac7a1b00db86e63a992aab8c63a07224b098d1
alextjj <254334386@qq.com>
2022-09-01 10:15:29 +0800

    add chat xlsx data

---
+++ messageGo/xlsxData/chat.go
@@ -0,0 +1,25 @@
+package jsonData
+
+import "time"
+
+// 频道类型
+type ChatChannelType int32
+
+const (
+	ChatChannelTypeUnknown ChatChannelType = 0
+	// 系统
+	ChatChannelTypeSystem ChatChannelType = 1
+	// 世界
+	ChatChannelTypeWorld ChatChannelType = 2
+	// 附近
+	ChatChannelTypeNearby ChatChannelType = 3
+	// 私聊
+	ChatChannelTypePrivate ChatChannelType = 4
+)
+
+type ChatSettings struct {
+	Id        uint            `gorm:"primaryKey;autoIncrement" json:"id,string"`
+	ChatType  ChatChannelType `json:"chatType"`
+	Cd        int32           `json:"cd"`
+	CreatedAt time.Time       `json:"createdAt"` // 过期判断条件
+}

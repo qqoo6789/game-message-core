7948b932b94b7916f0538b1cf4ca3d44cc8c5224
alextjj <254334386@qq.com>
2022-09-09 20:58:22 +0800

    move jsonData to grpc method eventdata

--- messageGo/grpc/methodData/service_data.go
+++ messageGo/grpc/methodData/service_data.go
@@ -1,8 +1,10 @@
-package jsonData
+package methodData
 
 import "game-message-core/proto"
 
+// 服务信息用于服务 注册和释放
 type ServiceData struct {
+	MsgVersion  int64             `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
 	Id          int64             `json:"id"`
 	Name        string            `json:"name"`
 	AppId       string            `json:"appId"`

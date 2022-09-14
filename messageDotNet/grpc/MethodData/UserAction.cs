c65a395cf65ae8b930bcfbc82d15375eef47ed09
alextjj <254334386@qq.com>
2022-09-14 11:15:27 +0800

    feat user grpc action

--- messageDotNet/grpc/MethodData/UserAction.cs
+++ messageDotNet/grpc/MethodData/UserAction.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /// player exit game 
 /// </summary>
 [Serializable]
-internal class PlayerLeaveGameInput
+internal class UserLeaveGameInput
 {
     // 消息版本号 值为毫秒时间戳
     public long MsgVersion;
@@ -23,13 +23,10 @@ internal class PlayerLeaveGameInput
 /// agent service forward client message response
 /// </summary>
 [Serializable]
-internal class PlayerLeaveGameOutput
+internal class UserLeaveGameOutput
 {
-    // 消息版本号 值为毫秒时间戳
-    public long MsgVersion;
-    public string AgentAppId;
-    public long UserId;
     public bool Success;
+    public string ErrMsg;
 
     public string ToJson()
     {

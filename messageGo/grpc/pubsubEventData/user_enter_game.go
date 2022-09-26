dc400cc5349e6734f305439619048863638257b3
alextjj <254334386@qq.com>
2022-09-26 16:00:46 +0800

    update user enter map event data

--- messageGo/grpc/pubsubEventData/user_enter_game.go
+++ messageGo/grpc/pubsubEventData/user_enter_game.go
@@ -1,15 +1,27 @@
 package pubsubEventData
 
-import (
-	base_data "game-message-core/grpc/baseData"
-	"game-message-core/proto"
-)
-
 type UserEnterGameEvent struct {
-	MsgVersion        int64                        `json:"msgVersion"`        // 消息版本号 值为毫秒时间戳
-	SceneServiceAppId string                       `json:"sceneServiceAppId"` //
-	MapId             int32                        `json:"mapId"`
-	BaseData          base_data.GrpcPlayerBaseData `json:"baseData"`
-	Position          proto.Vector3                `json:"position"`
-	Dir               proto.Vector3                `json:"dir"`
+	MsgVersion        int64   `json:"msgVersion"`        // 消息版本号 值为毫秒时间戳
+	SceneServiceAppId string  `json:"sceneServiceAppId"` //
+	MapId             int32   `json:"mapId"`
+	UserId            int64   `json:"userId"`
+	Name              string  `json:"name"`
+	RoleId            int32   `json:"roleId"`
+	Gender            string  `json:"gender"`
+	RoleIcon          string  `json:"roleIcon"`
+	Guide             bool    `json:"guide"`
+	Eyebrow           int32   `json:"eyebrow"`
+	Mouth             int32   `json:"mouth"`
+	Eye               int32   `json:"eye"`
+	Face              int32   `json:"face"`
+	Hair              int32   `json:"hair"`
+	Glove             int32   `json:"glove"`
+	Clothes           int32   `json:"clothes"`
+	Pants             int32   `json:"pants"`
+	X                 float32 `json:"x"`
+	Y                 float32 `json:"y"`
+	Z                 float32 `json:"z"`
+	DirX              float32 `json:"dirX"`
+	DirY              float32 `json:"dirY"`
+	DirZ              float32 `json:"dirZ"`
 }

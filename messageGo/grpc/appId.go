d78c089f50eaf2bc40af1d0a51a86ca40a496e05
alextjj <254334386@qq.com>
2022-09-28 17:05:00 +0800

    fix meland service app id

--- messageGo/grpc/appId.go
+++ messageGo/grpc/appId.go
@@ -2,10 +2,10 @@ package grpc
 
 type AppId string
 
-const AppIdMelandServiceManager AppId = "meland_service_manager"
-const AppIdMelandServiceMain AppId = "meland_service_main"
-const AppIdMelandServiceAccount AppId = "meland_service_account"
-const AppIdMelandServiceTask AppId = "meland_service_task"
-const AppIdMelandServiceChat AppId = "meland_service_chat"
-const AppIdMelandServiceSceneXX AppId = "meland_service_scene_xx" // xx为动态ID
-const AppIdMelandServiceAgentXX AppId = "meland_service_agent_xx" // xx为动态ID
+const AppIdMelandServiceManager AppId = "meland-service-manager"
+const AppIdMelandServiceMain AppId = "meland-service-main"
+const AppIdMelandServiceAccount AppId = "meland-service-account"
+const AppIdMelandServiceTask AppId = "meland-service-task"
+const AppIdMelandServiceChat AppId = "meland-service-chat"
+const AppIdMelandServiceSceneXX AppId = "meland-service-scene-xx" // xx为动态ID
+const AppIdMelandServiceAgentXX AppId = "meland-service-agent-xx" // xx为动态ID

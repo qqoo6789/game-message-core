0fafcc488b81feedeef8e15a0deb783878278072
alextjj <254334386@qq.com>
2022-10-12 14:56:48 +0800

    fix xlsx table (reward drop task taskList) data type

--- messageGo/xlsxTableData/task_list.go
+++ messageGo/xlsxTableData/task_list.go
@@ -10,26 +10,13 @@ type TaskListTableRow struct {
 	Level           int32     `json:"level"`
 	System          int32     `json:"system"`
 	IncludeTaskJson string    `gorm:"type:text" json:"includeTask"`
-	RewardItemsJson string    `gorm:"type:text" json:"rewardItemsJson"`
+	RewardId        int32     `json:"rewardId"`
 	RewardExp       int32     `json:"rewardExp"`
 	ProgressReset   bool      `json:"progressReset"`
 	NeedMELD        int32     `json:"needMELD"`
 	CreatedAt       time.Time `json:"createdAt"` // 过期判断条件,
 
 	IncludeTask *TaskObjectList `gorm:"-" json:"-"`
-	RewardItems *TaskObjectList `gorm:"-" json:"-"`
-}
-
-func (p *TaskListTableRow) SetRewardItems(objs *TaskObjectList) error {
-	bs, err := marshal(objs)
-	p.RewardItemsJson = string(bs)
-	return err
-}
-func (p *TaskListTableRow) GetRewardItems() *TaskObjectList {
-	if p.RewardItems == nil {
-		p.RewardItems, _ = unMarshal(p.RewardItemsJson)
-	}
-	return p.RewardItems
 }
 
 func (p *TaskListTableRow) SetIncludeTask(objs *TaskObjectList) error {

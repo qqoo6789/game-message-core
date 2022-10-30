65202d8f4088c5950da4283805294c75a4c7de30
alextjj <254334386@qq.com>
2022-10-30 17:50:11 +0800

    feat service unregister event

---
+++ messageGo/grpc/baseData/service.go
@@ -0,0 +1,16 @@
+package base_data
+
+import "game-message-core/proto"
+
+// service data
+type ServiceData struct {
+	AppId       string            `json:"appId"`
+	ServiceType proto.ServiceType `json:"serviceType"`
+	Host        string            `json:"host"`
+	Port        int32             `json:"port"`
+	MapId       int32             `json:"mapId"`
+	Online      int32             `json:"online"`
+	MaxOnline   int32             `json:"maxOnline"`
+	CreatedAt   int64             `json:"createdAt"`
+	UpdatedAt   int64             `json:"updatedAt"`
+}

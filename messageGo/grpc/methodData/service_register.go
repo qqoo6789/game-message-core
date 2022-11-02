9fcb12fd0fd7306472276f2a1b0cc3a0c9c7191d
alextjj <254334386@qq.com>
2022-11-02 14:38:44 +0800

    rename service register input output name for golang code

--- messageGo/grpc/methodData/service_register.go
+++ messageGo/grpc/methodData/service_register.go
@@ -5,13 +5,13 @@ import (
 )
 
 // 服务信息用于服务 注册和释放
-type ServiceDataInput struct {
+type ServiceRegisterInput struct {
 	MsgVersion int64                 `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
 	Service    base_data.ServiceData `json:"service"`    //
 }
 
 // 服务信息用于服务 注册和释放
-type ServiceDataOutput struct {
+type ServiceRegisterOutput struct {
 	MsgVersion int64 `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
 	Success    bool  `json:"success"`
 }

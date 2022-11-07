6b42fe294c9434be55065b9ba6dea9ac249e1483
alextjj <254334386@qq.com>
2022-11-07 17:34:48 +0800

    fix task table DBData types

--- messageGo/xlsxTableData/task.go
+++ messageGo/xlsxTableData/task.go
@@ -3,83 +3,123 @@ package xlsxTable
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"game-message-core/proto"
 	"time"
 )
 
 // ----------- make task xlsx used types --------------------------
-type TaskXlsxRowOption struct {
+type TaskXlsxOption struct {
 	OptionType proto.TaskOptionType `json:"optionType"`
 	Value      string               `json:"value"`
 }
 
 type TaskXlsxOptions struct {
-	Options []TaskXlsxRowOption `json:"options"`
+	Options []TaskXlsxOption `json:"options"`
 }
 
 //--------------- task db table used types --------------------------
 
-type TaskTableOptionParam struct {
-	Param1 int32 `json:"param1"`
-	Param2 int32 `json:"param2"`
-	Param3 int32 `json:"param3"`
-	Param4 int32 `json:"param4"`
-	Param5 int32 `json:"param5"`
+type TaskRowOption struct {
+	OptionType proto.TaskOptionType `json:"optionType"`
+	Param1     int32                `json:"param1"`
+	Param2     int32                `json:"param2"`
+	Param3     int32                `json:"param3"`
+	Param4     int32                `json:"param4"`
+	Chance     int32                `json:"chance"` // 权重值
 }
 
-type TaskTableOption struct {
-	OptionType      proto.TaskOptionType   `json:"optionType"`
-	RandomExclusive int32                  `json:"randomSum"` // 通常为10000
-	RandList        []TaskTableOptionParam `json:"randList"`
-}
-
-type TaskTableRowOptionList struct {
-	Options []TaskTableOption `json:"options"`
+type TaskRowOptionList struct {
+	// 权重和
+	ChanceSum int32 `json:"chanceSum"`
+	// 子项列表
+	Options []TaskRowOption `json:"options"`
 }
 
 type TaskTableRow struct {
-	UId         uint   `gorm:"primaryKey;autoIncrement" json:"uid,string"`
-	Id          int32  `json:"id"`
-	Level       int32  `json:"level"`
-	Name        string `json:"name"`
-	SubSystem   string `json:"subSystem"`
-	RewardId    int32  `json:"rewardId"`
-	RewardExp   int32  `json:"rewardExp"`
-	Difficulty  int32  `json:"difficulty"`
-	NextTaskId  int32  `json:"nextTaskId"`
-	OptionsJson string `gorm:"type:text" json:"optionsJson"`
+	UId                  uint   `gorm:"primaryKey;autoIncrement" json:"uid,string"`
+	Id                   int32  `json:"id"`
+	Level                int32  `json:"level"`
+	Name                 string `json:"name"`
+	SubSystem            string `json:"subSystem"`
+	RewardId             int32  `json:"rewardId"`
+	RewardExp            int32  `json:"rewardExp"`
+	Difficulty           int32  `json:"difficulty"`
+	DesignateOptionsJson string `gorm:"type:text" json:"designateOptionsJson"`
+	ChanceOptionsJson    string `gorm:"type:text" json:"chanceOptionsJson"`
 
 	CreatedAt time.Time `json:"createdAt"` // 过期判断条件,
 
-	Options *TaskTableRowOptionList `gorm:"-" json:"-"`
+	DesignateOptions *TaskRowOptionList `gorm:"-" json:"-"`
+	ChanceOptions    *TaskRowOptionList `gorm:"-" json:"-"`
 }
 
-func (p *TaskTableRow) SetOptions(options *TaskTableRowOptionList) error {
-	if options == nil {
-		return errors.New("options is nil")
-	}
-	if len(options.Options) < 1 {
-		return errors.New("options is empty")
+func (p *TaskTableRow) SetDesignateOptions(designateOptions *TaskRowOptionList) error {
+	if designateOptions == nil {
+		return errors.New("designateOptions is nil")
 	}
 
-	bs, err := json.Marshal(options)
+	bs, err := json.Marshal(designateOptions)
 	if err != nil {
 		return err
 	}
 
-	p.Options = options
-	p.OptionsJson = string(bs)
+	p.DesignateOptions = designateOptions
+	p.DesignateOptionsJson = string(bs)
 	return nil
 }
 
-func (p *TaskTableRow) GetOptions() (*TaskTableRowOptionList, error) {
-	if p.Options == nil {
-		options := &TaskTableRowOptionList{}
-		err := json.Unmarshal([]byte(p.OptionsJson), options)
+func (p *TaskTableRow) GetDesignateOptions() (*TaskRowOptionList, error) {
+	if p.DesignateOptions == nil {
+		options := &TaskRowOptionList{}
+		err := json.Unmarshal([]byte(p.DesignateOptionsJson), options)
 		if err != nil {
 			return nil, err
 		}
-		p.Options = options
+		p.DesignateOptions = options
 	}
-	return p.Options, nil
+	return p.DesignateOptions, nil
+}
+
+func (p *TaskTableRow) SetChanceOptions(chanceOptions *TaskRowOptionList) error {
+	if chanceOptions == nil {
+		return errors.New("designateOptions is nil")
+	}
+
+	bs, err := json.Marshal(chanceOptions)
+	if err != nil {
+		return err
+	}
+
+	p.ChanceOptions = chanceOptions
+	p.ChanceOptionsJson = string(bs)
+	return nil
+}
+
+func (p *TaskTableRow) GetChanceOptions() (*TaskRowOptionList, error) {
+	if p.ChanceOptions == nil {
+		options := &TaskRowOptionList{}
+		err := json.Unmarshal([]byte(p.ChanceOptionsJson), options)
+		if err != nil {
+			return nil, err
+		}
+		p.ChanceOptions = options
+	}
+	return p.ChanceOptions, nil
+}
+
+func (p *TaskTableRow) Check() error {
+	chanceOptionsExist := false
+	designateOptionsExist := false
+
+	if p.ChanceOptions != nil || len(p.ChanceOptions.Options) > 0 {
+		chanceOptionsExist = true
+	}
+	if p.DesignateOptions != nil || len(p.DesignateOptions.Options) > 0 {
+		designateOptionsExist = true
+	}
+	if !chanceOptionsExist && !designateOptionsExist {
+		return fmt.Errorf("task [%d] options is empty", p.Id)
+	}
+	return nil
 }

b148bb6e2533ced08dae2f7b6b4c285c6c200c09
alextjj <254334386@qq.com>
2022-11-15 14:53:25 +0800

    task finish event add reward item data

--- messageGo/grpc/pubsubEventData/task_finish.go
+++ messageGo/grpc/pubsubEventData/task_finish.go
@@ -1,10 +1,14 @@
 package pubsubEventData
 
-import "game-message-core/proto"
+import (
+	base_data "game-message-core/grpc/baseData"
+	"game-message-core/proto"
+)
 
 type TaskFinishEvent struct {
-	MsgVersion   int64              `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
-	UserId       int64              `json:"userId"`
-	TaskListType proto.TaskListType `json:"taskListType"`
-	TaskId       int32              `json:"taskId"`
+	MsgVersion   int64                        `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
+	UserId       int64                        `json:"userId"`
+	TaskListType proto.TaskListType           `json:"taskListType"`
+	TaskId       int32                        `json:"taskId"`
+	RewardItem   []base_data.GrpcItemBaseInfo `json:"rewardItem"`
 }
--- messageGo/grpc/pubsubEventData/task_list_finish.go
+++ messageGo/grpc/pubsubEventData/task_list_finish.go
@@ -1,9 +1,13 @@
 package pubsubEventData
 
-import "game-message-core/proto"
+import (
+	base_data "game-message-core/grpc/baseData"
+	"game-message-core/proto"
+)
 
 type TaskListFinishEvent struct {
-	MsgVersion   int64              `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
-	UserId       int64              `json:"userId"`
-	TaskListType proto.TaskListType `json:"taskListType"`
+	MsgVersion   int64                        `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
+	UserId       int64                        `json:"userId"`
+	TaskListType proto.TaskListType           `json:"taskListType"`
+	RewardItem   []base_data.GrpcItemBaseInfo `json:"rewardItem"`
 }

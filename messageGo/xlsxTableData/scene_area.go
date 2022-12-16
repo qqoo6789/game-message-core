6a3db94462fa6f268c8e773abe48582a86dce365
alextjj <254334386@qq.com>
2022-12-16 15:13:42 +0800

    scene area table add column 'SceneType'

--- messageGo/xlsxTableData/scene_area.go
+++ messageGo/xlsxTableData/scene_area.go
@@ -5,5 +5,6 @@ import "time"
 type SceneAreaRow struct {
 	Id        int32     `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name      string    `json:"name"`
+	SceneType string    `json:"sceneType"`
 	CreatedAt time.Time `json:"createdAt"` // 过期判断条件
 }

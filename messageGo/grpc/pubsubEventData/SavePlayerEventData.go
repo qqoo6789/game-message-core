b2c4659c0f1a1bf8287f3a6e6d27d6bb15371bd0
alextjj <254334386@qq.com>
2022-12-16 15:05:25 +0800

    fix save player data event for check is dungeon

--- messageGo/grpc/pubsubEventData/SavePlayerEventData.go
+++ messageGo/grpc/pubsubEventData/SavePlayerEventData.go
@@ -1,16 +1,18 @@
 package pubsubEventData
 
+import (
+	base_data "game-message-core/grpc/baseData"
+)
+
 type SavePlayerEventData struct {
-	MsgVersion        int64   `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
-	UserId            int64   `json:"userId"`
-	AgentServiceAppId string  `json:"agentAppId"`
-	MapId             int32   `json:"mapId"`
-	SceneServiceAppId string  `json:"sceneServiceAppId"`
-	PosX              float32 `json:"posX"`
-	PosY              float32 `json:"posY"`
-	PosZ              float32 `json:"posZ"`
-	CurHP             int32   `json:"curHP"`
-	DirX              float32 `json:"dirX"`
-	DirY              float32 `json:"dirY"`
-	DirZ              float32 `json:"dirZ"`
+	MsgVersion     int64                 `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
+	FormService    base_data.ServiceData `json:"formService"`
+	UserId         int64                 `json:"userId"`
+	PosX           float32               `json:"posX"`
+	PosY           float32               `json:"posY"`
+	PosZ           float32               `json:"posZ"`
+	CurHP          int32                 `json:"curHP"`
+	DirX           float32               `json:"dirX"`
+	DirY           float32               `json:"dirY"`
+	DirZ           float32               `json:"dirZ"`
 }

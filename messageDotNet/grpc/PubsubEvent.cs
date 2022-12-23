310706a3060aab82d074efb52b5d7db48c48af79
alextjj <254334386@qq.com>
2022-12-23 17:12:30 +0800

    home granary GRPC msg

--- messageDotNet/grpc/PubsubEvent.cs
+++ messageDotNet/grpc/PubsubEvent.cs
@@ -92,4 +92,12 @@ public static class SubscriptionEvent
     /// user level update event
     /// </summary>
     public const string SAVE_HOME_DATA = "EventTopicSaveHomeData";
+    /// <summary>
+    /// home Granary Stockpile Event
+    /// </summary>
+    public const string GRANARY_STOCKPILE = "EventTopicGranaryStockpile";
+
+
+
 }
+

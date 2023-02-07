c085b1617e7d8ebcba572aefeba7717d0c0e694c
alextjj <254334386@qq.com>
2023-02-07 15:58:15 +0800

    add talent tree event column name

--- messageGo/grpc/pubsubEventData/talent.go
+++ messageGo/grpc/pubsubEventData/talent.go
@@ -12,9 +12,9 @@ type UpdateTalentEvent struct {
 }
 
 type AddTalentExpEvent struct {
-	MsgVersion        int64                     `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
-	SceneServiceAppId string                    `json:"sceneServiceAppId"`
-	MapId             int32                     `json:"mapId"`
-	UserId            int64                     `json:"userId"`
-	AddExps           []base_data.GrpcTalentExp `json:"addExps"`
+	MsgVersion   int64                     `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
+	ServiceAppId string                    `json:"serviceAppId"`
+	MapId        int32                     `json:"mapId"`
+	UserId       int64                     `json:"userId"`
+	AddExps      []base_data.GrpcTalentExp `json:"addExps"`
 }

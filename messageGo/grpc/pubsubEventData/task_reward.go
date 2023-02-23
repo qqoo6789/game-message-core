307efc19e72c7af3421b932ae68d5a8ddf068ae2
alextjj <254334386@qq.com>
2023-02-23 22:33:19 +0800

    fix task grpc message

--- messageGo/grpc/pubsubEventData/task_reward.go
+++ messageGo/grpc/pubsubEventData/task_reward.go
@@ -1,11 +1,11 @@
 package pubsubEventData
 
+import base_data "game-message-core/grpc/baseData"
+
 type UserTaskRewardEvent struct {
-	MsgVersion     int64 `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
-	UserId         int64 `json:"userId"`
-	Exp            int32 `json:"exp"`
-	ItemCid        int32 `json:"itemCid"`
-	Num            int32 `json:"num"`
-	Quality        int32 `json:"quality"`
-	TaskListReward bool  `json:"taskListReward"`
+	MsgVersion     int64                        `json:"msgVersion"` // 消息版本号 值为毫秒时间戳
+	UserId         int64                        `json:"userId"`
+	Exps           []base_data.GrpcTalentExp    `json:"exps"`
+	Items          []base_data.GrpcItemBaseInfo `json:"items"`
+	TaskListReward bool                         `json:"taskListReward"`
 }

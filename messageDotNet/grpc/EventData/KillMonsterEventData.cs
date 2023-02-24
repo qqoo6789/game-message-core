38a122b5f7f6979c84ec3a922e7fff6e71bae47b
alextjj <254334386@qq.com>
2023-02-24 19:36:02 +0800

    kill monster add ownerId

--- messageDotNet/grpc/EventData/KillMonsterEventData.cs
+++ messageDotNet/grpc/EventData/KillMonsterEventData.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 /// kill monster event json 交互数据结构
 /// </summary>
 [Serializable]
-public class KillMonsterEventData : EventDataBase
+public class KillMonsterEventData
 {
     public long MsgVersion;
     public string SceneServiceAppId;
+    public int MapId;
+    public long OwnerId;
+    public long KillerId;
+    public float PosX;
+    public float PosY;
+    public float PosZ;
     public int MonsterCid;
     public string MonsterName;
     public GrpcItemBaseInfo[] DropList;

325b6128c69f34cd30663a3fc3f73ad6323fa11d
a412555466 <412555466@qq.com>
2023-05-10 12:29:26 +0800

    mr change

--- messageDotNet/runtime/ProtoUtil.cs
+++ messageDotNet/runtime/ProtoUtil.cs
@@ -18,38 +18,26 @@ namespace ProtoBuf.Runtime
         {
             // 获取消息包envelope的长度
             int envelopLength = envelope.CalculateSize();
-            // 根据长度转头字节数组
-            byte[] headerBytes = BitConverter.GetBytes(envelopLength);
-            if (packetHeaderLength < 0 || (packetHeaderLength > 0 && headerBytes.Length != packetHeaderLength))
-            {
-                Debug.Fail($"protobuf Serialize head size error,cur={headerBytes.Length} expected={packetHeaderLength}");
-                return null;
-            }
-
             // 创建（头长度+内容长度）的字节数组
             byte[] destination = new byte[packetHeaderLength + envelopLength];
-            // 选取内容区域，写入数据
-            Span<byte> envelopBytes = new(destination, packetHeaderLength, destination.Length - packetHeaderLength);
-            envelope.WriteTo(envelopBytes);
-
             // 头部区域写入数据
             if (packetHeaderLength > 0)
             {
+                byte[] headerBytes = BitConverter.GetBytes(envelopLength);
+                if (headerBytes.Length != packetHeaderLength)
+                {
+                    Debug.Fail($"protobuf Serialize head size error,cur={headerBytes.Length} expected={packetHeaderLength}");
+                    return null;
+                }
                 for (int i = 0; i < packetHeaderLength; i++)
                 {
                     destination[i] = headerBytes[i];
                 }
             }
-            return destination;
-        }
-        /// <summary>
-        /// 把envelope 编码为（包长+包内容）字节数组
-        /// </summary>
-        /// <param name="envelope">协议对象</param>
-        public static void EncodeToBytes(GameMessageCore.Envelope envelope, int packetLength, byte[] msgBody)
-        {
-            Span<byte> envelopBytes = new(msgBody, 0, packetLength);
+            // 选取内容区域，写入数据
+            Span<byte> envelopBytes = new(destination, packetHeaderLength, destination.Length - packetHeaderLength);
             envelope.WriteTo(envelopBytes);
+            return destination;
         }
         /// <summary>
         /// 解码协议数据 内容部分（不包括头长） 
